8f24bfdaeb7be7664878e270641c127689d3f27c
Franz Wollang <franzwollang@gmail.com>
2022-12-02 14:31:03 +0100

    Moved back to single file

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-"use strict";
 var _a;
-exports.__esModule = true;
-var utils_1 = require("./utils");
+// utils
+function coordToId(coord) {
+    var row = coord[0], col = coord[1];
+    return "".concat(row, "-").concat(col);
+}
+function idToCoord(id) {
+    var _a = id.split("-").map(function (elem) { return Number(elem); }), row = _a[0], col = _a[1];
+    return [row, col];
+}
+// grab DOM elements
 var gameGrid = document.querySelector("#game-grid");
+// settings
 var gridSize = 3;
 var gridCellStyles = ["w-[200px]", "h-[200px]", "border", "border-black"];
+// creating game grid
 for (var row = 0; row < gridSize; row++) {
-	for (var col = 0; col < gridSize; col++) {
-		// create gridCell & add styling
-		var gridCell = document.createElement("div");
-		(_a = gridCell.classList).add.apply(_a, gridCellStyles);
-		gridCell.id = (0, utils_1.coordToId)([row, col]);
-		// append gridCell to game gameGrid
-		gameGrid.appendChild(gridCell);
-	}
+    for (var col = 0; col < gridSize; col++) {
+        // create gridCell & add styling
+        var gridCell = document.createElement("div");
+        (_a = gridCell.classList).add.apply(_a, gridCellStyles);
+        gridCell.id = coordToId([row, col]);
+        // append gridCell to game gameGrid
+        gameGrid.appendChild(gridCell);
+    }
 }
--- main.ts
+++ main.ts
@@ -1,12 +1,27 @@
-import { Coordinate } from "./types";
-import { idToCoord, coordToId } from "./utils";
+// types
+type Coordinate = [number, number];
 
+// utils
+function coordToId(coord: Coordinate): string {
+	const [row, col] = coord;
+
+	return `${row}-${col}`;
+}
+
+function idToCoord(id: string): Coordinate {
+	const [row, col] = id.split("-").map((elem) => Number(elem));
+
+	return [row, col];
+}
+
+// grab DOM elements
 const gameGrid = document.querySelector("#game-grid") as Element;
 
+// settings
 const gridSize = 3;
-
 const gridCellStyles = ["w-[200px]", "h-[200px]", "border", "border-black"];
 
+// creating game grid
 for (let row = 0; row < gridSize; row++) {
 	for (let col = 0; col < gridSize; col++) {
 		// create gridCell & add styling
